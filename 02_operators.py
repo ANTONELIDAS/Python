c75f2ebd288ec6f61ded8d5a5885ee6a33327bcf
Antonella <antopaoloni@gmail.com>
2022-10-07 20:30:30 +0100

    Fin de operators

---
+++ 02_operators.py
@@ -0,0 +1,51 @@
+### Operadores aritmeticos ###
+
+from queue import PriorityQueue
+
+
+print(3+4)
+print (3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 3) # este me da el modulo (osea el resto). Matematica a marzo 2023
+print(10 // 3) # da el resultado sin decimal
+
+
+print('Hola' + 'Python')
+# print('Hola' - 'Python') # Pues tio no se pueden sumar cadenas de texto.
+print('Hola ' * 5)
+
+
+### Operadores comparativos ###
+'''
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 == 4)
+print(3 != 4)
+print(3 > 4 == 2)
+'''
+
+print('hola' > 'phyton')
+print('hola' < 'phyton')
+print('aaa' > 'zzz') # Compara el orden alfabetico 
+print(len('hola') >= len('chau')) # Para comparar la longitud de los caracteres
+
+### Operadores lógicos ###
+
+print(3 > 4 and 'hola' > 'phyton')
+print(3 > 4 or 'hola' > 'phyton')
+print(not(3 > 4)) 
+
+
+
+
+
+
+
+
+
+
+
+
